c128166f79dd84166cb5f186c29331a0e47b1996
April Darger <april.m.darger@gmail.com>
2020-12-18 00:05:52 -0700

    Tested endpoints.

--- api/server.test.js
+++ api/server.test.js
@@ -2,40 +2,55 @@ const request = require('supertest');
 const server = require('./server');
 const db = require('../data/dbConfig');
 
+const April = { username: 'April', password: 'PassThis' };
+
 beforeAll(async () => {
   await db.migrate.rollback();
   await db.migrate.latest();
-})
+});
 beforeEach(async () => {
   await db('users').truncate();
-})
+});
 afterAll(async () => {
   await db.destroy();
-})
+});
 
 describe('endpoints', () => {
   describe('[POST] /api/auth/register', () => {
-    it('', async () => {
-      
-    })
-    it('', async () => {
-
-    })
-  })
+    it('posts a new user and returns newly registered user data', async () => {
+      const res = await request(server).post('/api/auth/register').send(April);
+      expect(res.body.id).toBe(1);
+      expect(res.body.username).toBe('April');
+    });
+    it('responds with "username taken" when user attempts to register twice', async () => {
+      await request(server).post('/api/auth/register').send(April);
+      const res = await request(server).post('/api/auth/register').send(April);
+      expect(JSON.stringify(res.body)).toMatch(/username taken/);
+    });
+  });
   describe('[POST] /api/auth/login', () => {
+    it('welcmes the user upon logging in', async () => {
+      await request(server).post('/api/auth/register').send(April);
+      const res = await request(server).post('/api/auth/login').send(April);      
+      expect(JSON.stringify(res.body)).toMatch(/welcome, April/);
+    });
     it('', async () => {
-      
-    })
-    it('', async () => {
-
-    })
-  })
+      const res = await request(server).post('/api/auth/login').send();
+      expect(JSON.stringify(res.body)).toMatch(/username and password required/);
+    });
+  });
   describe('[GET] /api/jokes', () => {
+    it('prevents access without token', async () => {
+     await db('users').insert(April);
+     const res = await request(server).get('/api/jokes');
+     expect(JSON.stringify(res.body)).toMatch(/token required/);
+    });
     it('', async () => {
-      
-    })
-    it('', async () => {
+      await request(server).post('/api/auth/register').send(April);
 
-    })
-  })
-})
+      const user = await request(server).post('/api/auth/login').send(April); 
+      const res = await request(server).get('/api/jokes').set('Authorization', `${user.body.token}`);
+      expect(res.text).toMatch(/Because he had no guts/);
+    });
+  });
+});
