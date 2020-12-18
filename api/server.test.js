7185703593eb79c73b9f5acdf8263f224897c67e
April Darger <april.m.darger@gmail.com>
2020-12-17 19:55:34 -0700

    Layed outline for MVP tests required. Nothing actually being tested yet, outline only.

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,41 @@
-// Write your tests here
-it('sanity', () => {
-  expect(true).toBe(false)
+const request = require('supertest');
+const server = require('./server');
+const db = require('../data/dbConfig');
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+})
+beforeEach(async () => {
+  await db('users').truncate();
+})
+afterAll(async () => {
+  await db.destroy();
+})
+
+describe('endpoints', () => {
+  describe('[POST] /api/auth/register', () => {
+    it('', async () => {
+      
+    })
+    it('', async () => {
+
+    })
+  })
+  describe('[POST] /api/auth/login', () => {
+    it('', async () => {
+      
+    })
+    it('', async () => {
+
+    })
+  })
+  describe('[GET] /api/jokes', () => {
+    it('', async () => {
+      
+    })
+    it('', async () => {
+
+    })
+  })
 })
