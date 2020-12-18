87b280da1a51afee6b1a33321d91796d408de80c
April Darger <april.m.darger@gmail.com>
2020-12-17 22:52:01 -0700

    Wrote success for registration endpoint.

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const User = require('./users-model');
-const { payloadExists, usernameUnique, usernameExists } = require('../middleware/registration');
+const { payloadExists, usernameUnique, usernameExists, passwordCheck, makeToken } = require('../middleware/registration');
 
-router.post('/register', payloadExists, usernameUnique, async (req, res) => {
+router.post('/register', payloadExists, usernameUnique, async (req, res) => {  
   try{
     const hash = bcrypt.hashSync(req.body.password, 10);
     const newUser = await User.add({
@@ -40,8 +40,19 @@ router.post('/register', payloadExists, usernameUnique, async (req, res) => {
   */
 });
 
-router.post('/login', payloadExists, usernameExists, (req, res) => {
-  res.end('implement login, please!');
+router.post('/login', payloadExists, usernameExists, passwordCheck, async (req, res) => {
+  try{
+    const rows = await User.findBy({ username: req.body.username });
+    const user = rows[0];
+    const token = makeToken(user);
+    res.status(200).json({
+      message: `welcome, ${user.username}`,
+      token
+    });
+  } catch (error) {
+    res.status(500).json({ message:error.message });
+  }
+  
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
